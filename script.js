99b1b0cbbeac8b7791331b7ffe69c33d70364ee0
InnerPause <innerpause@gmail.com>
2025-03-14 13:07:13 -0400

    Update script.js
    update to include PWA functionality for iPhone users

--- script.js
+++ script.js
@@ -201,3 +201,13 @@ exportTweetBtn.addEventListener('click', () => {
 });
 
 updatePreview();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/TweetPreview/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered');
+    })
+    .catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
